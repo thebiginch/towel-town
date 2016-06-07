f99719f3b360466e7ab7d90f2fae449f3c86d33e
Zachary Levine <zrlevine@gmail.com>
2016-06-07 14:23:16 -0400

    bug fix

--- server/app/routes/orderRouter.js
+++ server/app/routes/orderRouter.js
@@ -2,7 +2,7 @@
 
 var express = require('express');
 var router = express.Router();
-var db = require('../../../db/index');
+var db = require('../../db/index');
 var Order = db.model('order');
 
 router.get('/', function (req, res, next) {
