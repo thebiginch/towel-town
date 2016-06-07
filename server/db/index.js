d510b65be58395b4ca4e02bb4041f4cfa4ecfd93
Zachary Levine <zrlevine@gmail.com>
2016-06-07 11:01:32 -0400

    added associations

--- server/db/index.js
+++ server/db/index.js
@@ -3,11 +3,22 @@ var db = require('./_db');
 
 require('./models/user')(db);
 require('./models/order')(db);
+require('./models/reviews')(db);
+require('./models/towel')(db);
 
-var User = require('./models/user');
-var Order = require('./models/order');
+
+
+var User = db.model('user');
+var Order = db.model('order');
+var Review = db.model('review');
+var Towel = db.model('towel');
+
 
 User.hasMany(Order, { foreignKey : 'user_id' });
-Order.belongsTo(User, { as : 'user' });
+Order.belongsTo(User);
+Order.hasMany(Towel);
+User.hasMany(Review, { foreignKey : 'user_id'});
+Towel.hasMany(Review,{foreignKey : 'review_id'});
+Review.belongsTo(Towel);
 
 module.exports = db;
