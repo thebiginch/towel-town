acc33b30475f173e79421e98cddf737985088666
Zachary Levine <zrlevine@gmail.com>
2016-06-08 16:39:10 -0400

    towel angular stuff

--- browser/js/common/factories/RandomGreetings.js
+++ browser/js/common/factories/RandomGreetings.js
@@ -5,18 +5,7 @@ app.factory('RandomGreetings', function () {
     };
 
     var greetings = [
-        'Hello, world!',
-        'At long last, I live!',
-        'Hello, simple human.',
-        'What a beautiful day!',
-        'I\'m like any other project, except that I am yours. :)',
-        'This empty string is for Lindsay Levine.',
-        'こんにちは、ユーザー様。',
-        'Welcome. To. WEBSITE.',
-        ':D',
-        'Yes, I think we\'ve met before.',
-        'Gimme 3 mins... I just grabbed this really dope frittata',
-        'If Cooper could offer only one piece of advice, it would be to nevSQUIRREL!',
+        'Don\'t forget to bring a towel'
     ];
 
     return {
