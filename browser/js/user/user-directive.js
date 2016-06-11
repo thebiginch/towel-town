4f59b1e1c559670de4c7e1254e295be0154f776a
Zachary Levine <zrlevine@gmail.com>
2016-06-11 15:55:13 -0400

    fix orderRouter and user directive

--- browser/js/user/user-directive.js
+++ browser/js/user/user-directive.js
@@ -1,30 +1,16 @@
 app.config(function($stateProvider) {
 
-    // $stateProvider.state('user', {
-    //     url: '/users/:userId',
-    //     templateUrl: 'js/user/user.html',
-    //     controller: 'UserController',
-    //     resolve: {
-    //         user: function($stateParams, UserFactory) {
-    //             return UserFactory.fetchOne($stateParams.userId);
-    //         }
-    //     }
-    // });
-
     $stateProvider.state('profile', {
         url: '/tacos',
         templateUrl: 'js/user/user.html',
         controller: 'UserController',
-    });
-
-    $stateProvider.state('profile.default', {
+    })
+    .state('profile.default', {
         url: '/profile',
         templateUrl: 'js/user/user-home.html',
         controller: 'UserController'
-
-    });
-
-    $stateProvider.state('profile.orders', {
+    })
+    .state('profile.orders', {
         url: '/orders/:userId',
         templateUrl: 'js/user/user-orders.html',
         controller: 'UserOrdersController',
@@ -33,15 +19,12 @@ app.config(function($stateProvider) {
                 return UserFactory.fetchUserOrders(Session.user.id);
             }
         }
-
-    });
-
-    $stateProvider.state('profile.reviews', {
+    })
+    .state('profile.reviews', {
         url: '/reviews',
         templateUrl: 'js/user/user-reviews.html',
         controller: 'UserController'
     });
-
 
 });
 
@@ -57,18 +40,13 @@ app.factory('UserFactory', function($http,Session) {
         .then(getData);
     };
 
-    UserFactory.isLoggedIn = function(){
-        return $http.get('/session')
-        .then(getData)
-    };
-
     UserFactory.fetchUserOrders = function(id) {
-        return $http.post('/api/orders/orderByUser', {userId: id})
+        return $http.get('/api/orders/order/' + id)
         .then(getData);
     };
 
     UserFactory.fetchUserReviews = function(id) {
-        return $http.post('/api/orders/orderByUser', {userId: id})
+        return $http.get('/api/reviews/users/' + id)
         .then(getData);
     };
 
@@ -77,12 +55,8 @@ app.factory('UserFactory', function($http,Session) {
 
 app.controller('UserController', function($scope, UserFactory,Session) {
     $scope.user = Session.user;
-});
-
-app.controller('UserOrdersController', function($scope,userOrders) {
+}).controller('UserOrdersController', function($scope,userOrders) {
     $scope.userOrders = userOrders;
-});
-
-app.controller('UserReviewsController', function($scope,userReviews) {
+}).controller('UserReviewsController', function($scope,userReviews) {
     $scope.userReviews = userReviews;
 });
