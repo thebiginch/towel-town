c0ba922adcd4d58458a225a784e1c3b9ed9a5cf5
Zachary Levine <zrlevine@gmail.com>
2016-06-13 10:32:55 -0400

    editing towel list filters

--- browser/js/towel/list/towel.list.controller.js
+++ browser/js/towel/list/towel.list.controller.js
@@ -1,4 +1,32 @@
 app.controller('allTowelController', function($scope, TowelFactory, allTowels) {
-	
     $scope.towels = allTowels;
+
+    $scope.towelOptions = {
+    	type: ['Beach','Bath','Gym','Face'],
+    	color: ['Fushia','Coral'],
+    	material: ['Pima Cotton','Bamboo']
+    };
+
+    $scope.filteredOptions = {
+    };
+
+    $scope.taco = {};
+
+    $scope.print = function(){
+    	console.dir($scope.taco);
+    };
+    $scope.toggle = function(category){
+        $scope.filteredOptions[category] = !$scope.filteredOptions[category];
+    
+        if(!$scope.filteredOptions[category]) delete $scope.taco[category];
+    };
+
+    $scope.filterFunction = function(obj,taco){
+    	
+    	
+
+    	 return true;
+    };
+
 });
+
